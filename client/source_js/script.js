01473d7b0563ea6707130a637e0f2067e3ebd9b6
William Fazio <faziow14@users.noreply.github.com>
2016-05-04 04:27:11 +0000

    Carousel code on homepage, still having some loading problems.

--- client/source_js/script.js
+++ client/source_js/script.js
@@ -1,16 +1,16 @@
-var divs = document.getElementsByClassName('alert');
-for(var i=0; i<divs.length; i++) {
-    divs[i].addEventListener("click", highlightThis);
-    /*
-    divs[i].addEventListener("click", highlightThis, true);
-    divs[i].addEventListener("click", highlightThis, false);*/
-}
-
-function highlightThis(event) {
-    //event.stopPropagation();
-
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+// This function initializes the carousel on the homepage.
+$(document).ready(function(){
+	alert("script.js ready function");
+	$("#carousel").owlCarousel({
+		singleItem: true,
+		slideSpeed: 300,
+      	navigation: true,
+      	navigationText: ["Back", "Next"],
+      	paginationNumbers: true,
+      	paginationSpeed: 400,
+      	autoHeight: true,
+      	transitionStyle: "fadeUp",
+  		loop:  true,
+    	margin:  10,
+  	});
+});
